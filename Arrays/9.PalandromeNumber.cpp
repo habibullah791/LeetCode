3677b9e2054f2ed38587ad5a60a25bef3f09fd75
habibullah791 <whoishabib791@gmail.com>
2023-01-03 01:15:13 +0500

    Compelete Promlem 4, 9, 13 ( Arrays )

---
+++ Arrays/9.PalandromeNumber.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool isPalindrome(int x)
+{
+    string str = to_string(x);
+    string temp = str;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        temp[i] = str[str.length() - i - 1];
+
+    }
+
+    if (temp == str)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int main()
+{
+    isPalindrome(789);
+}
