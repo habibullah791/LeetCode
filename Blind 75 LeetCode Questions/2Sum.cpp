b6d6c314fee09673931643b06e38f43ba29fe2e3
habibullah791 <whoishabib791@gmail.com>
2023-05-02 02:46:09 +0500

    1st Blind 75 Leet code problems

---
+++ Blind 75 LeetCode Questions/2Sum.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    vector<int> end;
+    unordered_map<int, int> map;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int complement = target - nums[i];
+        if (map.find(complement) != map.end())
+        {
+            end.push_back(map[complement]);
+            end.push_back(i);
+            return end;
+        }
+        map[nums[i]] = i;
+    }
+    return end;
+}
+
+int main()
+{
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> end = twoSum(nums, target);
+    for (int i = 0; i < end.size(); i++)
+    {
+        cout << end[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
